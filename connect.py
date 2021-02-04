620ab799a2afebe4b3bacf8fd5700938cded8886
ANALYTICS_ULTS <62049023+ultsdl@users.noreply.github.com>
2021-02-04 10:00:09 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -3,8 +3,8 @@ import psycopg2,pandas
 
 mydb = psycopg2.connect(database="kannur_corporation", 
                               user="analytics_view", 
-                              password="Ulgis@1Ults", 
-                              host="103.133.180.27", 
+                              password="", 
+                              host="", 
                               port="5432")
 
 def execute_querry(querry,connection=mydb):
